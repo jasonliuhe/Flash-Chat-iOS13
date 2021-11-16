51c5218a32279f705f4ab355ec493c3c3d1e91aa
jasonliuhe <walkerliuhe@gmail.com>
2021-11-16 16:54:21 -0500

    day finish

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by He Liu on 2021-11-16.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
